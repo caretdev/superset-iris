a7bac0f0120b026ae41f067a4f891925dfe97fcd
Dmitry Maslennikov <mrdaimor@gmail.com>
2023-05-02 17:31:34 +0400

    upgrade to IRIS only edition

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     install_requires=[
-        "sqlalchemy-iris~=0.8.0",
+        "sqlalchemy-iris~=0.10.0",
     ],
     entry_points={
         'superset.db_engine_specs': ['iris=superset_iris.engine:IRISEngineSpec']
